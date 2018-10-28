9361c44b742a63845ae1854fc331dadf1a332316
GermanDZ <germ@ndz.com.ar>
2018-10-28 10:00:14 +0100

    solution for bottles replenishment

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,11 +1,26 @@
 class Bottles
   def verse(number)
-    "#{bottles_count(number)} of beer on the wall, #{bottles_count(number)} of beer.\n" \
-    "Take #{number > 1 ? "one" : "it"} down and pass it around, #{bottles_count(number-1)} of beer on the wall.\n"
+    new_number = number.zero? ? 99 : number - 1
+    first_sentence(number) + second_sentence(number, new_number)
+  end
+
+  def first_sentence(number)
+    "#{bottles_count(number).capitalize} of beer on the wall, " \
+    "#{bottles_count(number)} of beer.\n"
+  end
+
+  def second_sentence(number, new_number)
+    "#{action(number)}, " \
+    "#{bottles_count(new_number)} of beer on the wall.\n"
   end
 
   def bottles_count(number)
     return "no more bottles" if number.zero?
     "#{number} bottle#{"s" if number > 1}"
+  end
+
+  def action(number)
+    return "Go to the store and buy some more" if number.zero?
+    "Take #{number > 1 ? "one" : "it"} down and pass it around"
   end
 end
