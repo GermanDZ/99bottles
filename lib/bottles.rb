07892badaf2c9542437413fa2686e4a4fa606624
GermanDZ <germ@ndz.com.ar>
2018-10-28 09:39:53 +0100

    solution when there are only 1 bottle remaining on the wall

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,6 +1,6 @@
 class Bottles
   def verse(number)
     "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" \
-    "Take one down and pass it around, #{number-1} bottles of beer on the wall.\n"
+    "Take one down and pass it around, #{number-1} bottle#{"s" if number > 2} of beer on the wall.\n"
   end
 end
