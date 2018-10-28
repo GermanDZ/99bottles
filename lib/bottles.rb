da2cc2be7a9f830011143226b67f70a9f34444c9
GermanDZ <germ@ndz.com.ar>
2018-10-28 09:44:31 +0100

    solution when the last bottle is took down.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,6 +1,11 @@
 class Bottles
   def verse(number)
-    "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" \
-    "Take one down and pass it around, #{number-1} bottle#{"s" if number > 2} of beer on the wall.\n"
+    "#{bottles_count(number)} of beer on the wall, #{bottles_count(number)} of beer.\n" \
+    "Take #{number > 1 ? "one" : "it"} down and pass it around, #{bottles_count(number-1)} of beer on the wall.\n"
+  end
+
+  def bottles_count(number)
+    return "no more bottles" if number.zero?
+    "#{number} bottle#{"s" if number > 1}"
   end
 end
